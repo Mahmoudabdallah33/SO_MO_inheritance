3ebad95fa9984dd45afcaf143726fc9428711528
Mahmoudabdallah33 <153030672+Mahmoudabdallah33@users.noreply.github.com>
2024-03-30 23:15:45 +0200

    Add files via upload

---
+++ MO_SO_Inheritance/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name' : 'SalesOreder_Manufacturing_Inheritance',
+    'version' : '1.2',
+    'summary': ' ',
+    'sequence': 0,
+    'description': """ ***  """,
+    'category': 'Inheritance',
+    'website': ' ',
+    'depends' : ['sale', 'mrp'],
+    'data': [
+        'views/MO_view.xml',
+    ],
+    'demo': [     ],
+    'installable': True,
+    'application': True,
+    'assets': {
+
+    },
+    'license': 'LGPL-3',
+}
